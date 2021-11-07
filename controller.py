d3c07c8b5f88461179d88336df0d33a5b2fce9dc
Tyler Westland <tylerofthewest@gmail.com>
2021-11-07 17:24:06 -0500

    Setup to clone a PS3 controller directly.

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ __author__="Tyler Westland"
 import argparse
 # https://python-evdev.readthedocs.io/en/latest/index.html
 import evdev
+import json
 import os
 import sys
 import time
@@ -13,11 +14,13 @@ from typing import List
 
 
 class GameCubeController():
-    def __init__(self, controller_name:str=None):
+    def __init__(self, clone_parent:str, controller_name:str=None):
         if controller_name is None:
             controller_name = "python--GameCube"
 
-        self.ui = evdev.uinput.UInput(name=controller_name)
+        self.ui = evdev.uinput.UInput.from_device(
+                clone_parent,
+                name=controller_name)
 
     def close(self):
         self.ui.close()
@@ -48,6 +51,36 @@ class GameCubeController():
         # Synchronize the lifts
         self.ui.syn()
 
+    def push_sticks(self, sticks:List[int], values:List[int], seconds:float=0.5)\
+            -> None:
+        """Push button for specified time
+        Parameters
+        ----------
+        sticks: List[int] (evdev.ecodes)
+            Sticks to manipulate, and then reset.
+        values: List[int] 
+            Values to set the sticks to. Should be the same size as sticks
+        seconds: float = 0.5
+            Seconds to hold the button down for
+        """
+        # Push the sticks down
+        for i in range(len(sticks)):
+            self.ui.write(evdev.events.EV_ABS, sticks[i], values[i])
+
+        # Synchronize the pushes
+        self.ui.syn()
+
+        # Wait
+        time.sleep(seconds)
+
+        # Move sticks back to center
+        # Since this is intended to clone a PS3 controller that's 127
+        for i in range(len(sticks)):
+            self.ui.write(evdev.events.EV_ABS, sticks[i], 127)
+
+        # Synchronize the lifts
+        self.ui.syn()
+
     # Right thumb buttons
     def push_a(self, seconds:float=0.5):
         """Push A button for specified time
@@ -56,7 +89,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_A], seconds)
+        self.push_buttons([evdev.ecodes.BTN_SOUTH], seconds)
 
     def push_b(self, seconds:float=0.5):
         """Push B button for specified time
@@ -65,7 +98,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_B], seconds)
+        self.push_buttons([evdev.ecodes.BTN_EAST], seconds)
 
     def push_x(self, seconds:float=0.5):
         """Push X button for specified time
@@ -74,7 +107,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_X], seconds)
+        self.push_buttons([evdev.ecodes.BTN_NORTH], seconds)
 
     def push_y(self, seconds:float=0.5):
         """Push Y button for specified time
@@ -83,7 +116,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_Y], seconds)
+        self.push_buttons([evdev.ecodes.BTN_WEST], seconds)
 
     # Trigger buttons
     def push_z(self, seconds:float=0.5):
@@ -93,7 +126,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_Z], seconds)
+        self.push_buttons([evdev.ecodes.BTN_TR], seconds)
 
     def push_left_trigger(self, seconds:float=0.5):
         """Push left trigger for specified time
@@ -102,7 +135,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_TL], seconds)
+        self.push_buttons([evdev.ecodes.BTN_TL2], seconds)
 
     def push_right_trigger(self, seconds:float=0.5):
         """Push right trigger for specified time
@@ -111,7 +144,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_TR], seconds)
+        self.push_buttons([evdev.ecodes.BTN_TR2], seconds)
 
     # DPAD
     def push_dpad_up(self, seconds:float=0.5):
@@ -158,16 +191,16 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_FORWARD], seconds)
+        self.push_sticks([evdev.ecodes.ABS_Y], [0], seconds)
 
-    def push_control_stick_back(self, seconds:float=0.5):
+    def push_control_stick_down(self, seconds:float=0.5):
         """Push Control Stick back for specified time
         Parameters
         ----------
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_BACK], seconds)
+        self.push_sticks([evdev.ecodes.ABS_Y], [255], seconds)
 
     def push_control_stick_left(self, seconds:float=0.5):
         """Push Control Stick left for specified time
@@ -176,7 +209,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_LEFT], seconds)
+        self.push_sticks([evdev.ecodes.ABS_X], [0], seconds)
 
     def push_control_stick_right(self, seconds:float=0.5):
         """Push Control Stick right for specified time
@@ -185,7 +218,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_RIGHT], seconds)
+        self.push_sticks([evdev.ecodes.ABS_X], [255], seconds)
 
     # C Stick
     def push_c_stick_up(self, seconds:float=0.5):
@@ -195,7 +228,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_NORTH], seconds)
+        self.push_sticks([evdev.ecodes.ABS_RY], [0], seconds)
 
     def push_c_stick_down(self, seconds:float=0.5):
         """Push C Stick down for specified time
@@ -204,7 +237,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_SOUTH], seconds)
+        self.push_sticks([evdev.ecodes.ABS_RY], [255], seconds)
 
     def push_c_stick_left(self, seconds:float=0.5):
         """Push C Stick left for specified time
@@ -212,7 +245,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_WEST], seconds)
+        self.push_sticks([evdev.ecodes.ABS_RX], [0], seconds)
 
     def push_c_stick_right(self, seconds:float=0.5):
         """Push C Stick right for specified time
@@ -221,7 +254,7 @@ class GameCubeController():
         seconds: float = 0.5
             Seconds to hold the button down for
         """
-        self.push_buttons([evdev.ecodes.BTN_EAST], seconds)
+        self.push_sticks([evdev.ecodes.ABS_RX], [255], seconds)
 
 
 def parse_arguments(args=None) -> None:
@@ -235,25 +268,28 @@ def parse_arguments(args=None) -> None:
     parser = argparse.ArgumentParser(
             description="Create and control a virtual gamecube controller.",
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("clone_parent", help="Controller to clone from.")
     parser.add_argument("-cn", "--controller_name", help="Controller name.")
     args = parser.parse_args(args=args)
     return args
 
 
-def main(controller_name:str=None) -> None:
+def main(clone_parent:str, controller_name:str=None) -> None:
     """Main function.
 
     Parameters
     ----------
+    clone_parent: str
+        Controller to clone from.
     controller_name: str
-        Controller name
+        Controller name.
     Raises
     ------
     FileNotFoundError
         Means that the input file was not found.
     """
 
-    GC = GameCubeController(controller_name)
+    GC = GameCubeController(clone_parent, controller_name)
 
     option = None
     while(option != "q"):
